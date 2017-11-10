6c5d4b1ba0aae9f8658aab9a5f10c095d0fbd667
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-11-10 13:39:37 -0800

    Update run-cvmfs-install

--- run-cvmfs-install.sh
+++ run-cvmfs-install.sh
@@ -7,7 +7,7 @@ export P8DEPKATYDIDBUILD=build-2017-10-18
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # make the dependencies build directory
-mkdir -p /cvmfs/hep.pnnl.gov/project8/dependencies/${P8DEPKATYDIDBUILD}
+mkdir -p /cvmfs/hep.pnnl.gov/project8/dependencies-katydid/${P8DEPKATYDIDBUILD}
 
 # copy in the necessary scripts
 cp ${SCRIPT_DIR}/setup.sh /cvmfs/hep.pnnl.gov/project8/dependencies-katydid/${P8DEPKATYDIDBUILD}/setup.sh
@@ -16,6 +16,6 @@ cp ${SCRIPT_DIR}/download_pkg.sh /cvmfs/hep.pnnl.gov/project8/dependencies-katyd
 cp ${SCRIPT_DIR}/install.sh /cvmfs/hep.pnnl.gov/project8/dependencies-katydid/${P8DEPKATYDIDBUILD}/install.sh
 
 # setup the environment, and run the download and install scripts
-source /cvmfs/hep.pnnl.gov/project8/dependencies/${P8DEPKATYDIDBUILD}/setup.sh
-/cvmfs/hep.pnnl.gov/project8/dependencies/${P8DEPKATYDIDBUILD}/download_pkg.sh
-/cvmfs/hep.pnnl.gov/project8/dependencies/${P8DEPKATYDIDBUILD}/install.sh
+source /cvmfs/hep.pnnl.gov/project8/dependencies-katydid/${P8DEPKATYDIDBUILD}/setup.sh
+/cvmfs/hep.pnnl.gov/project8/dependencies-katydid/${P8DEPKATYDIDBUILD}/download_pkg.sh
+/cvmfs/hep.pnnl.gov/project8/dependencies-katydid/${P8DEPKATYDIDBUILD}/install.sh
